7eed418549d47ac298370caf524c683799defe8f
sudarshan295 <125974462+sudarshan295@users.noreply.github.com>
2023-04-09 12:32:37 +0530

    Create script.js

---
+++ 08. Adding a Method to a Prototype/script.js
@@ -0,0 +1,15 @@
+/*
+Create a prototype object called Student with a property name. Add a method called printDetails 
+to the prototype that logs the string "Hello, my name is {name}" to the console. Instantiate a 
+Student object with the name "Mithun" and call the printDetails method.
+*/
+
+const Student = function(name){
+    this.name = name;
+    }
+    
+    Student.prototype.printDetails =function(){
+        console.log( `Hello, my name is ${this.name}`)
+    }
+    const student = new Student("Mithun");
+    student.printDetails();
