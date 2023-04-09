2fbe90b7d4a355ccbfa9aaa0cb261dac51804317
sudarshan295 <125974462+sudarshan295@users.noreply.github.com>
2023-04-09 12:25:02 +0530

    Create script.js

---
+++ 04. Employee Class Challenge/script.js
@@ -0,0 +1,21 @@
+/*
+Create a class called Employee with three properties: name, position, and salary. 
+The class should have a method called getSalary that returns the employee's salary. 
+Instantiate an instance of the Employee class and call the getSalary method.
+*/
+
+class Employee{
+    name ;
+    position;
+    salary;
+    constructor(n, p, s){
+        this.name= n;
+        this.position= p;
+        this.salary= s;
+    }
+    getSalary(){
+        return this.salary;
+    }
+}
+const employee1= new Employee("Sudarshan", "Web developer", 80000);
+console.log(employee1.getSalary());
